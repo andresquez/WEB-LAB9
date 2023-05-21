061bbfcb1c631acc7d4aeea8cf80adad77c67d3b
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-05-20 23:27:48 -0600

    Vite Startup

---
+++ quesoCalc/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div />
+  );
+}
+
+export default App;
---
+++ quesoCalc/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [eslint(),react()],
+  test: {
+    setupFiles: './src/setupTests.js',
+    globals: true,
+    environment: 'jsdom'
+  }
+})
